668cb93807e595fcf0512a3b3391e07bc0a771ea
Karas <karasgupta@gmail.com>
2025-12-12 17:43:48 +0800

    Use allowed_origins_patterns for dynamic CORS2

--- lms-rpl/config/cors.php
+++ lms-rpl/config/cors.php
@@ -2,20 +2,21 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
     'allowed_methods' => ['*'],
 
-    // [UBAH INI] Kosongkan yang spesifik
+    // Kosongkan yang spesifik
     'allowed_origins' => [],
 
-    // [ISI INI] Gunakan Pola Bintang (*).
-    // Laravel akan otomatis mendeteksi domain pengirim dan mengizinkannya,
-    // Serta memperbolehkan Credentials/Login berjalan lancar.
+    // [JURUS PAMUNGKAS] Pakai Bintang (*) di Pattern
+    // Ini memaksa Laravel menerima request dari MANAPUN.
     'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    
-    // Wajib true untuk login
+
     'supports_credentials' => true,
 ];
