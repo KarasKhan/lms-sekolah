8638c660bfa671c893af7cba13b6151dc9478567
Karas <karasgupta@gmail.com>
2025-12-12 16:09:10 +0800

    Update config vite dan perbaikan logika status

--- lms-frontend/vite.config.js
+++ lms-frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. Naikkan batas peringatan jadi 1000kb (1MB) biar gak berisik
+    chunkSizeWarningLimit: 1000,
+    
+    // 2. Pecah file vendor (library) agar tidak menumpuk di satu file
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return 'vendor';
+                }
+            }
+        }
+    }
+  }
 })
